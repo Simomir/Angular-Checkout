633ae2bc5e4942fc818b67bb24dfe50cd41b27f7
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-12-01 23:12:37 +0200

    service logic

--- src/app/services/link.service.ts
+++ src/app/services/link.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinkService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(code: string): Observable<any> {
+    return this.http.get(`${environment.api}/links/${code}`);
+  }
 }
