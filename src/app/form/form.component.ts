fc4afa86fd7854716229a5c99881ecad8e4ce21e
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-12-01 23:42:18 +0200

    getting link data

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,13 +9,14 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class FormComponent implements OnInit {
   code!: string;
+  user!: any;
 
   constructor(private linkService: LinkService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.code = this.route.snapshot.params['code'];
     this.linkService.get(this.code).subscribe({
-      next: value => { console.log(value); }
+      next: value => { this.user = value[0].user; }
     });
   }
 
