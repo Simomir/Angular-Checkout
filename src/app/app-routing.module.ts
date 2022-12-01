89d79acecc19c266a41de78929c1da2e7b85c855
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-12-01 23:04:43 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SuccessComponent } from "./success/success.component";
+import { ErrorComponent } from "./error/error.component";
+import { FormComponent } from "./form/form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'success', component: SuccessComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: ':code', component: FormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
